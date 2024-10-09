d03e88cce9ca01b70e39aff9d7434f24109b03f5
Juwon Hong <wndnjs9127@gmail.com>
2024-10-09 10:54:38 -0700

    Initial Commit

---
+++ 10-class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  10-class
+//
+//  Created by 홍주원 on 10/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
